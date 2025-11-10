44cab95e13f21d29b7c6caa06bf9e23f21886989
Eldritch-hub <abdhakeemadewale@gmail.com>
2025-11-10 01:08:06 +0100

    new

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin:
-  ["*"],
+  origin: ["https://silver-spoonn.vercel.app", "http://localhost:5173"],
   methods: ["GET", "POST"],
   credentials: true
 }));
